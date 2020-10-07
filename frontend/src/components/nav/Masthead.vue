491fd3b0e4f0f68a9518598023df085e712e8dae
Petr Hnyk <[hnyk.petr@centrum.cz]>
2020-10-07 18:40:56 +0200

    Navbar navigation animations added + burger

---
+++ frontend/src/components/nav/Masthead.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="masthead">
+    <div class="container">
+      <div class="masthead-heading text-uppercase">Hoď se do stylu.</div>
+      <div class="masthead-subheading">ESHOP ČVUT FAKULTY DOPRAVNÍ</div>
+      <!--<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>-->
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/nav/Navbar.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="d-flex justify-content-around navbar navbar-expand-lg fixed-top" id="mainNav" :style="scrolled ? 'background-color:rgba(0,0,0,0.9); transition: 0.5s ease' : 'transition: 0.5s ease'">
+    <div>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="../../assets/images/logofd.png" style="height: 70px" alt="LOGO FD" /></a>
+    </div>
+    <div style="padding-top: 5px" v-if="resized">
+      <ul class="navbar-nav text-uppercase">
+        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">MUŽI</a></li>
+        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">ŽENY</a></li>
+        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">OSTATNÍ</a></li>
+      </ul>
+    </div>
+    <div style="padding-top: 5px; text-align: center" v-if="resized">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#"><fai icon="shopping-cart" size="lg" /><span class="icon_span">Košík</span></a></li>
+        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#"><fai icon="user" size="lg" /><span class="icon_span">Přihlásit se</span></a></li>
+      </ul>
+    </div>
+    <div @click="openMenu" style="padding-top: 5px" v-if="!resized">
+      <div class="navbar-toggler navbar-toggler-right" :class="menuClass">
+        <div class="menu-btn__burger"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        scrolled: false,
+        resized: true,
+        menuOpen: false,
+        menuClass: 'menu-btn',
+      }
+    },
+    methods: {
+      openMenu() {
+        if (this.menuOpen === false) {
+          this.menuClass = "menu-btn open";
+          this.showMenu();
+        } else {
+          this.menuClass = "menu-btn";
+          this.hideMenu();
+        }
+      },
+      showMenu() {
+        setTimeout(() => {
+          this.menuOpen = true
+        },500);
+      },
+      hideMenu() {
+        setTimeout(() => {
+          this.menuOpen = false
+        },500);
+      },
+      handleScroll: function() {
+        this.scrolled = window.scrollY >= 150;
+      },
+      handleResize() {
+        this.resized = window.outerWidth >= 991;
+      }
+    },
+    created: function() {
+      window.addEventListener('scroll', this.handleScroll);
+      window.addEventListener('resize', this.handleResize);
+      return this.handleResize();
+    }
+  }
+</script>
+
+<style scoped>
+  .icon_span {
+    display: block;
+    padding-top: 5px;
+    font-weight: lighter;
+  }
+
+  .menu-btn {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    cursor: pointer;
+    transition: all .5s ease-in-out;
+    /* border: 3px solid #fff; */
+  }
+  .menu-btn__burger {
+    width: 30px;
+    height: 4px;
+    background: #fff;
+    border-radius: 5px;
+    transition: all .5s ease-in-out;
+  }
+  .menu-btn__burger::before,
+  .menu-btn__burger::after {
+    content: '';
+    position: absolute;
+    width: 30px;
+    height: 4px;
+    background: #fff;
+    border-radius: 5px;
+    transition: all .5s ease-in-out;
+  }
+  .menu-btn__burger::before {
+    transform: translateY(-10px);
+  }
+  .menu-btn__burger::after {
+    transform: translateY(10px);
+  }
+  /* ANIMATION */
+  .menu-btn.open .menu-btn__burger {
+    transform: translateX(-50px);
+    background: transparent;
+    box-shadow: none;
+  }
+  .menu-btn.open .menu-btn__burger::before {
+    transform: rotate(45deg) translate(25px, -25px);
+  }
+  .menu-btn.open .menu-btn__burger::after {
+    transform: rotate(-45deg) translate(25px, 25px);
+  }
+</style>
