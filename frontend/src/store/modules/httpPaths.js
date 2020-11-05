d992bcb60918ad474eab2abd090f6aec00eb75c7
Petr Hnyk <[hnyk.petr@centrum.cz]>
2020-11-05 17:43:18 +0100

    Frontend API address and port env update

--- frontend/src/store/modules/httpPaths.js
+++ frontend/src/store/modules/httpPaths.js
@@ -2,12 +2,12 @@ export default {
     namespaced: true,
     state() {
         return {
-            other: `${process.env.API_ADDRESS || 'http://127.0.0.1'}:${process.env.PORT || 3000}/product`,
-            man: `${process.env.API_ADDRESS || 'http://127.0.0.1'}:${process.env.PORT || 3000}/product/man`,
-            woman: `${process.env.API_ADDRESS || 'http://127.0.0.1'}:${process.env.PORT || 3000}/product/woman`,
-            singleProduct: `${process.env.API_ADDRESS || 'http://127.0.0.1'}:${process.env.PORT || 3000}/product/`,
-            login: `${process.env.API_ADDRESS || 'http://127.0.0.1'}:${process.env.PORT || 3000}/user/login`,
-            register: `${process.env.API_ADDRESS || 'http://127.0.0.1'}:${process.env.PORT || 3000}/user`
+            other: `${process.env.VUE_APP_API_ADDRESS || 'http://127.0.0.1'}:${process.env.VUE_APP_PORT || 3000}/product`,
+            man: `${process.env.VUE_APP_API_ADDRESS || 'http://127.0.0.1'}:${process.env.VUE_APP_PORT || 3000}/product/man`,
+            woman: `${process.env.VUE_APP_API_ADDRESS || 'http://127.0.0.1'}:${process.env.VUE_APP_PORT || 3000}/product/woman`,
+            singleProduct: `${process.env.VUE_APP_API_ADDRESS || 'http://127.0.0.1'}:${process.env.VUE_APP_PORT || 3000}/product/`,
+            login: `${process.env.VUE_APP_API_ADDRESS || 'http://127.0.0.1'}:${process.env.VUE_APP_PORT || 3000}/user/login`,
+            register: `${process.env.VUE_APP_API_ADDRESS || 'http://127.0.0.1'}:${process.env.VUE_APP_PORT || 3000}/user`
         }
     }
 };
